ebf79c2101db1857cef4017a7d36e0366c723658
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-09-07 08:20:18 -0300

    Conectando com o banco mongo

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+mongoose.connect('mongodb://localhost:1234/node-api',{ useNewUrlParser: true, useUnifiedTopology: true }) 
+
+app.get('/',(req,res)=>{
+    res.send('Olá Mundo!')
+})
+
+app.listen(3000,()=>{
+    console.log("Servidor está rodando em: http://localhost:3000")
+})
