a68731ea29158142f32fef56a16f2211c4da5564
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-09-07 08:26:16 -0300

    Adicionando require-dir para autoload

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 const app = express()
 mongoose.connect('mongodb://localhost:1234/node-api',{ useNewUrlParser: true, useUnifiedTopology: true }) 
 
+requireDir('./src/models')
 
 app.get('/',(req,res)=>{
     res.send('Olá Mundo!')
