fa37a9ffe9d98b680d618f70bc364979aff9f721
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-09-07 14:30:51 -0300

    Cors

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+const cors = require('cors')
 
 const app = express()
-
 mongoose.connect('mongodb://localhost:1234/node-api',{ useNewUrlParser: true, useUnifiedTopology: true }) 
 
 requireDir('./src/models')
 
 
+app.use(cors())
 app.use(express.json())
 app.use('/',require('./src/routes'))
 
